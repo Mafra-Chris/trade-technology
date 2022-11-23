438354f81d2899de8e77928aad4e464377726b8a
chris-mafra <mafradesouza.cs@gmail.com>
2022-11-23 17:59:11 -0300

    feat: function to teste api and refactor returns

--- src/services/footballAPI.ts
+++ src/services/footballAPI.ts
@@ -1,8 +1,24 @@
 import axios from "axios";
 
-const BASE_URL = 'https://v3.football.api-sports.io'
-const HOST = 'v3.football.api-sports.io'
+const BASE_URL = 'https://api-football-v1.p.rapidapi.com/v3'
+const HOST = 'api-football-v1.p.rapidapi.com'
 
+export async function testAPI(apiKey: string) {
+  try {
+    const response = await axios.get(`${BASE_URL}/timezone`, {
+      headers: {
+        'x-rapidapi-key': apiKey,
+        'x-rapidapi-host': HOST
+      }
+    })
+    if (response.status === 200) {
+      return true
+    }
+  } catch (error) {
+    return false
+  }
+
+}
 export async function getCountries(apiKey: string) {
   try {
     const response = await axios.get(`${BASE_URL}/countries`, {
@@ -11,37 +27,45 @@ export async function getCountries(apiKey: string) {
         'x-rapidapi-host': HOST
       }
     })
-    return response.data.response
+    return response.data.response.map((el: any) => {
+      return {
+        label: el.name,
+        value: el.name,
+        flag: el.flag,
+      }
+    })
   } catch (error) {
     console.log(error)
   }
 
 }
-export async function getLeagues(apiKey: string, countryCode: string) {
+export async function getLeagues(apiKey: string, country: string, season: number) {
   try {
     const response = await axios.get(`${BASE_URL}/leagues`, {
       headers: {
         'x-rapidapi-key': apiKey,
         'x-rapidapi-host': HOST
       },
-      params: { code: countryCode }
+      params: { country: country, season: season }
     })
-    return response.data.response
+    console.log(response)
+    return response.data.response.map((el: any) => { return el.league })
   } catch (error) {
     console.log(error)
   }
 
 }
-export async function getTeams(apiKey: string, leagueId: number) {
+export async function getTeams(apiKey: string, leagueId: number, season: number, country: string) {
   try {
     const response = await axios.get(`${BASE_URL}/teams`, {
       headers: {
         'x-rapidapi-key': apiKey,
         'x-rapidapi-host': HOST
       },
-      params: { league: leagueId }
+      params: { league: leagueId, season: season, country: country }
     })
-    return response.data.response
+    console.log(response)
+    return response.data.response.map((el: any) => { return el.team })
   } catch (error) {
     console.log(error)
   }
